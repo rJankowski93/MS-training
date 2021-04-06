d7b25d71c1854e18a865f25461376548b0ca38a5
Rafal Jankowski <rafal.jankowski@comarch.com>
2021-04-06 20:55:23 +0200

    03 Spring Web - rest-example - 2 (Swagger)

--- 04-Spring-Web/rest-example/src/main/java/com/org/rjankowski/ms/restexample/CustomerResource.java
+++ 04-Spring-Web/rest-example/src/main/java/com/org/rjankowski/ms/restexample/CustomerResource.java
@@ -1,5 +1,8 @@
 package com.org.rjankowski.ms.restexample;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,11 @@ public class CustomerResource {
     private final CustomerRepository customerRepository;
 
     @GetMapping("/customers")
+    @ApiOperation(value = "Get list of customers")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "firstName", value = "First name", dataType = "String", paramType = "query"),
+            @ApiImplicitParam(name = "lastName", value = "Last name", dataType = "String", paramType = "query")
+    })
     public ResponseEntity<List> getCustomers(@RequestParam(value = "firstName", required = false) String firstName, @RequestParam(value = "lastName", required = false) String lastName){
         if(firstName != null || lastName!= null){
             return new ResponseEntity<>(customerRepository.getListByFirstNameAndLastName(firstName,lastName), HttpStatus.OK);
@@ -20,6 +28,10 @@ public class CustomerResource {
         return new ResponseEntity<>(customerRepository.getList(), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Get customer")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "Id of customer", dataType = "Long", paramType = "path")
+    })
     @GetMapping("customers/{id}")
     public ResponseEntity<Customer> getCustomer(@PathVariable Long id){
         try{
@@ -32,12 +44,14 @@ public class CustomerResource {
     }
 
     @PostMapping("/customers")
+    @ApiOperation(value = "Add customer")
     public ResponseEntity createCustomer(@RequestBody Customer customer){
         customerRepository.createCustomer(customer);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @DeleteMapping("/customers")
+    @ApiOperation(value = "Delete customer")
     public ResponseEntity deleteCustomer(Customer customer){
         customerRepository.removeCustomer(customer);
         return new ResponseEntity<>(HttpStatus.OK);
