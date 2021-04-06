88d2ee5d08158bdab9e739c96467748823ef72d9
Rafal Jankowski <rafal.jankowski@comarch.com>
2021-04-06 20:43:20 +0200

    Generated example project in Spring Boot

---
+++ 03-Spring-Boot/generated-example-project/src/main/java/com/org/rjankowski/ms/generatedexampleproject/GeneratedExampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.org.rjankowski.ms.generatedexampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratedExampleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratedExampleProjectApplication.class, args);
+    }
+
+}
---
+++ 03-Spring-Boot/generated-example-project/src/test/java/com/org/rjankowski/ms/generatedexampleproject/GeneratedExampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.rjankowski.ms.generatedexampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratedExampleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
