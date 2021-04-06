811743b07f6b3aa4e6f635a2f205301bb2649612
Rafal Jankowski <rafal.jankowski@comarch.com>
2021-04-06 21:52:43 +0200

    04 Spring Data - h2-example - start

---
+++ 05-Spring-Data/h2-example/src/main/java/com/org/rjankowski/ms/h2example/H2ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.org.rjankowski.ms.h2example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2ExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H2ExampleApplication.class, args);
+    }
+
+}
---
+++ 05-Spring-Data/h2-example/src/test/java/com/org/rjankowski/ms/h2example/H2ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.rjankowski.ms.h2example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2ExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
