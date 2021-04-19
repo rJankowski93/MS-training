808c4796767876f2f6c8db20a6e9a512038aec30
Rafal Jankowski <rafal.jankowski@comarch.com>
2021-04-19 15:39:23 +0200

    09 Circult Breaker pattern - Hystrix - 4(Turbine)

---
+++ 09-Circuit-Breaker/turbine/src/test/java/com/org/rjankowski/ms/turbine/TurbineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.rjankowski.ms.turbine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurbineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
