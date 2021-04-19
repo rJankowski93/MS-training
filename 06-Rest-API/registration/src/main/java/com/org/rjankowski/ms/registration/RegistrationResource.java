ef85c8a539e93f4a86723f11636e808bc262f541
Rafal Jankowski <rafal.jankowski@comarch.com>
2021-04-19 14:11:22 +0200

    08 Config Server - Refresh config Registration

--- 06-Rest-API/registration/src/main/java/com/org/rjankowski/ms/registration/RegistrationResource.java
+++ 06-Rest-API/registration/src/main/java/com/org/rjankowski/ms/registration/RegistrationResource.java
@@ -1,8 +1,11 @@
 package com.org.rjankowski.ms.registration;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +16,9 @@ import org.springframework.web.client.RestTemplate;
 public class RegistrationResource {
 
     private final RestTemplate restTemplate;
+
+    @Autowired
+    private Environment env;
 
     @PostMapping("/register")
     public ResponseEntity registration(@RequestBody RegistrationRequest registrationRequest){
@@ -49,5 +55,10 @@ public class RegistrationResource {
             }
         }
         return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/version")
+    public String version(){
+        return env.getProperty("custom.api.version");
     }
 }
