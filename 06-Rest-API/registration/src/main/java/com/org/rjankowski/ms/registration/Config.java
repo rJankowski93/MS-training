2ee7f2b3ae509c2f3b380d0a73e0cb1c8c56f8d8
Rafal Jankowski <rafal.jankowski@comarch.com>
2021-04-19 14:27:54 +0200

    09 Circult Breaker pattern - Hystrix - 1(/close)

--- 06-Rest-API/registration/src/main/java/com/org/rjankowski/ms/registration/Config.java
+++ 06-Rest-API/registration/src/main/java/com/org/rjankowski/ms/registration/Config.java
@@ -1,11 +1,13 @@
 package com.org.rjankowski.ms.registration;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableHystrix
 public class Config {
 
     @Bean
--- 06-Rest-API/registration/src/main/java/com/org/rjankowski/ms/registration/RegistrationResource.java
+++ 06-Rest-API/registration/src/main/java/com/org/rjankowski/ms/registration/RegistrationResource.java
@@ -1,5 +1,7 @@
 package com.org.rjankowski.ms.registration;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class RegistrationResource {
@@ -21,23 +26,23 @@ public class RegistrationResource {
     private Environment env;
 
     @PostMapping("/register")
-    public ResponseEntity registration(@RequestBody RegistrationRequest registrationRequest){
+    public ResponseEntity registration(@RequestBody RegistrationRequest registrationRequest) {
         ResponseEntity<Customer[]> forEntity = restTemplate.getForEntity("http://Customers/customers", Customer[].class);
         for (Customer customer : forEntity.getBody()) {
-            if(customer.getFirstName().equals(registrationRequest.getFirstName()) && customer.getLastName().equals(registrationRequest.getLastName())){
+            if (customer.getFirstName().equals(registrationRequest.getFirstName()) && customer.getLastName().equals(registrationRequest.getLastName())) {
                 throw new RuntimeException();
             }
         }
-        Customer customer = new Customer(registrationRequest.getFirstName(),registrationRequest.getLastName(),false, registrationRequest.getAddresses());
+        Customer customer = new Customer(registrationRequest.getFirstName(), registrationRequest.getLastName(), false, registrationRequest.getAddresses());
         restTemplate.postForEntity("http://Customers/customers", customer, ResponseEntity.class);
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @PostMapping("/active")
-    public ResponseEntity active(@RequestBody ActivationRequest activationRequest){
+    public ResponseEntity active(@RequestBody ActivationRequest activationRequest) {
         ResponseEntity<Customer[]> forEntity = restTemplate.getForEntity("http://Customers/customers", Customer[].class);
         for (Customer customer : forEntity.getBody()) {
-            if(customer.getFirstName().equals(activationRequest.getFirstName()) && customer.getLastName().equals(activationRequest.getLastName())){
+            if (customer.getFirstName().equals(activationRequest.getFirstName()) && customer.getLastName().equals(activationRequest.getLastName())) {
                 customer.setActive(true);
                 restTemplate.postForEntity("http://Customers/customers", customer, ResponseEntity.class);
             }
@@ -46,10 +51,13 @@ public class RegistrationResource {
     }
 
     @PostMapping("/close")
-    public ResponseEntity close(@RequestBody ClosingRequest closingRequest){
+    @HystrixCommand(fallbackMethod = "fallbackClose", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500")
+    })
+    public ResponseEntity close(@RequestBody ClosingRequest closingRequest) {
         ResponseEntity<Customer[]> forEntity = restTemplate.getForEntity("http://Customers/customers", Customer[].class);
         for (Customer customer : forEntity.getBody()) {
-            if(customer.getFirstName().equals(closingRequest.getFirstName()) && customer.getLastName().equals(closingRequest.getLastName())){
+            if (customer.getFirstName().equals(closingRequest.getFirstName()) && customer.getLastName().equals(closingRequest.getLastName())) {
                 restTemplate.delete("http://Customers/customers", customer);
                 return new ResponseEntity(HttpStatus.OK);
             }
@@ -58,7 +66,14 @@ public class RegistrationResource {
     }
 
     @GetMapping("/version")
-    public String version(){
+    public String version() {
         return env.getProperty("custom.api.version");
+    }
+
+    List<ClosingRequest> closingRequestLis = new ArrayList<>();
+
+    public ResponseEntity fallbackClose(@RequestBody ClosingRequest closingRequest) {
+        closingRequestLis.add(closingRequest);
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
